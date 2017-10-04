7bdacbd8c9c3fc1d74352d77e369b56a6525293f
reiosedrik <reiosedrik@gmail.com>
2017-10-04 22:38:05 +0300

    weatherequest from api

--- src/main/java/request/WeatherRequest.java
+++ src/main/java/request/WeatherRequest.java
@@ -1,5 +1,11 @@
 package request;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class WeatherRequest {
 
     private String key;
@@ -7,9 +13,65 @@ public class WeatherRequest {
     private String city;
     private String unit;
 
-    public WeatherRequest(String city, String unit) {
 
+    private BufferedReader reader;
+
+    private java.net.URL requestURL;
+    private HttpURLConnection connection;
+    private String requestAddress;
+
+    private String temporaryStr;
+    private String response;
+
+    public WeatherRequest(String city, String unit) {
+        this.city = city;
+        this.unit = unit;
     }
+
+    public String getJSONFromURL() {
+        setupConnection();
+        readFromConnection();
+        disconnect();
+        return response;
+    }
+
+    private void disconnect() {
+        if (connection != null) {
+            connection.disconnect();
+        }
+    }
+
+    private void setupConnection() {
+        try {
+            requestURL = new URL(requestAddress);
+            connection = (HttpURLConnection) requestURL.openConnection();
+            connection.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = null;
+        }
+    }
+
+    private void readFromConnection() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((temporaryStr = reader.readLine()) != null) {
+                response = temporaryStr;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = null;
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     public String getKey() {
         return key;
--- src/test/java/testRequest.java
+++ src/test/java/testRequest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+import request.WeatherRequest;
+
+import static junit.framework.TestCase.assertFalse;
+
 public class testRequest {
+
+    private WeatherRequest weatherRequest;
+
+    @Before
+    public void setupWeatherRequest() {
+        weatherRequest = new WeatherRequest("Tallinn", "metric");
+    }
+
+    @Test
+    public void testResponseNotNull() {
+        assertFalse(weatherRequest.getJSONFromURL() == null);
+    }
 }
